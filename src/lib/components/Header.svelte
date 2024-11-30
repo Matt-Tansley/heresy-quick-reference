c5459d3b9c0e39e6871f6020b788823a313c142f
Matt-Tansley <30matthewt@gmail.com>
2024-11-30 18:01:58 +1300

    task-2: created header component, chose icon library

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { version } from '$app/environment';
+	import GameIconsWaxTablet from 'virtual:icons/game-icons/wax-tablet';
+</script>
+
+<div class="header-container center">
+	<h2><GameIconsWaxTablet style="font-size: 1.2em" /> Heresy Quick Reference</h2>
+	<p>
+		This app attempts to alphabetise all special rules for Horus Heresy 2.0 for quick and easy
+		reference during play.
+	</p>
+	<small>Version: {version}</small>
+</div>
+
+<style>
+	.header-container {
+		border-bottom-style: solid;
+		border-color: var(--pico-primary);
+		padding: 24px;
+		flex-direction: column;
+	}
+</style>
