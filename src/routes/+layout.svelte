ae109026f67aeb1047204da1e5d1fc94eb463c39
Matt T <30matthewt@gmail.com>
2024-11-30 18:07:04 +1300

    task-2: created header component, chose icon library (#1)
    * task-2: created header component, chose icon library
    
    * task-2: update readme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '$lib/styles/custom_pico.css'; // Customised Pico CSS
-	import '@picocss/pico/css/pico.fuchsia.min.css'; // This is how we change theme color https://picocss.com/docs/version-picker/pink
+	import '@picocss/pico/css/pico.pumpkin.min.css'; // This is how we change theme color https://picocss.com/docs/version-picker/pink
+	import '@picocss/pico/css/pico.colors.min.css';
 	import '$lib/styles/app.css'; // Completely custom styles
 </script>
 
