697cf29912031804af4cdc1684d84db06edad0bd
Matt T <30matthewt@gmail.com>
2024-11-30 18:32:41 +1300

    task-4: created accordion component (#2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte';
+	import Accordion from '$lib/components/Accordion.svelte';
+
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 </script>
 
 <Header></Header>
 
-<main class="container"></main>
+<main class="container">
+	{#each alphabet as letter}
+		<Accordion title={letter.toUpperCase()}></Accordion>
+	{/each}
+</main>
