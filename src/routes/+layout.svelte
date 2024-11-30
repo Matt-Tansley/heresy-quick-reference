517d4a91486a9164d245c71881f2cadd7ac78459
Matt-Tansley <30matthewt@gmail.com>
2024-11-30 16:35:00 +1300

    Task: 3 - initial project setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '$lib/styles/custom_pico.css'; // Customised Pico CSS
+	import '@picocss/pico/css/pico.fuchsia.min.css'; // This is how we change theme color https://picocss.com/docs/version-picker/pink
+	import '$lib/styles/app.css'; // Completely custom styles
+</script>
+
+<slot></slot>
