3a07e27325cc64480143088b1d4ff55f0e0902a4
Matt-Tansley <30matthewt@gmail.com>
2024-11-30 18:31:45 +1300

    task-4: created accordion component

---
+++ src/lib/components/Accordion.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let { title } = $props();
+</script>
+
+<details>
+	<summary class="accordion-title" role="button">{title}</summary>
+	<p>Info goes here</p>
+</details>
+
+<style>
+	.accordion-title {
+		font-weight: bold;
+		text-align: center;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '$lib/styles/custom_pico.css'; // Customised Pico CSS
-	import '@picocss/pico/css/pico.pumpkin.min.css'; // This is how we change theme color https://picocss.com/docs/version-picker/pink
+	import '@picocss/pico/css/pico.cyan.min.css'; // This is how we change theme color https://picocss.com/docs/version-picker/pink
 	import '@picocss/pico/css/pico.colors.min.css';
 	import '$lib/styles/app.css'; // Completely custom styles
 </script>
