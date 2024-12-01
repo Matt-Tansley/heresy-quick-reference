84f6c1320d9ff2fbcd345105376bbf2c5c0f182a
Matt T <30matthewt@gmail.com>
2024-12-01 21:16:10 +1300

    task-1: added JSON file with data from BattleScribe. Load function to load and use this data (#3)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import '$lib/styles/custom_pico.css'; // Customised Pico CSS
+	import '$lib/styles/custom-pico.css'; // Customised Pico CSS
 	import '@picocss/pico/css/pico.cyan.min.css'; // This is how we change theme color https://picocss.com/docs/version-picker/pink
 	import '@picocss/pico/css/pico.colors.min.css';
 	import '$lib/styles/app.css'; // Completely custom styles
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte';
 	import Accordion from '$lib/components/Accordion.svelte';
+	import type { PageData } from './$types';
 
 	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+	let { data }: { data: PageData } = $props();
+	const rules = data.rules; // Extract rules into it's own variable for simplicity
 </script>
 
 <Header></Header>
 
 <main class="container">
 	{#each alphabet as letter}
-		<Accordion title={letter.toUpperCase()}></Accordion>
+		<Accordion
+			title={letter.toUpperCase()}
+			rules={rules.filter((rule) => rule.name.toLowerCase().startsWith(letter))}
+		></Accordion>
 	{/each}
 </main>
