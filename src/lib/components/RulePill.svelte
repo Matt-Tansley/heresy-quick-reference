9b2b23cac6a9d511648cd3d15508deb49631709a
Matt-Tansley <30matthewt@gmail.com>
2024-12-01 22:04:21 +1300

    task-5: created rule pill component that has a modal

---
+++ src/lib/components/RulePill.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	let { rule } = $props();
+
+	let modal: HTMLDialogElement;
+
+	const toggleModal = () => {
+		console.log('button clicked');
+
+		modal.open ? closeModal() : openModal();
+		// modal && (modal.open ? closeModal(modal) : openModal(modal));
+	};
+
+	const openModal = () => {
+		modal.showModal();
+	};
+
+	const closeModal = () => {
+		modal.close();
+	};
+</script>
+
+<!-- Rule pill -->
+<article class="card" onclick={toggleModal}>{rule.name}</article>
+
+<!-- Modal -->
+<dialog bind:this={modal}>
+	<article>
+		<header>
+			<button aria-label="Close" rel="prev" onclick={toggleModal}></button>
+			<h3>{rule.name}</h3>
+		</header>
+		<p>
+			{rule.text}
+		</p>
+	</article>
+</dialog>
+
+<style>
+	.card {
+		background-color: var(--pico-color-grey-750);
+	}
+</style>
